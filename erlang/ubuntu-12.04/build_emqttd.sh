7ef2640c1b4d6223cbc5d69cf1fab82acf6cd745
thewawar <thewawar@gmail.com>
2016-11-06 10:03:20 +0000

    Update docker for linode-ubuntu

--- erlang/ubuntu-12.04/build_emqttd.sh
+++ erlang/ubuntu-12.04/build_emqttd.sh
@@ -2,7 +2,11 @@
 
 if [ -d "emqttd-relx" ]; then
     cd emqttd-relx
-    git pull
+    if [ -d ".git" ]; then
+        git pull
+    else
+        git clone --depth 2 https://github.com/emqtt/emqttd-relx.git .
+    fi
 else
     git clone --depth 2 https://github.com/emqtt/emqttd-relx.git
     cd emqttd-relx
@@ -11,4 +15,4 @@ make clean
 rm -rf _rel
 make
 make rel
-python2 /release.py
+python2 /mnt/release.py
